bc46fdb68644831c22f68ecf02420a54ce7ef11a
87Prashant <87KumarPrashant@gmail.com>
2022-07-18 20:52:04 +0530

    structure

---
+++ .history/src/pages/index_20220718205020.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Header from '../components/Header'
+import  from '../components/Main'
+import Footer from '../components/Footer'
+
+const Home: NextPage = () => {
+  return (
+    
+  )
+}
+
+export default Home
