08c4c659e19e585c667ad97698673154a4199952
87Prashant <87KumarPrashant@gmail.com>
2022-07-19 23:30:51 +0530

    nextjs router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,31 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Header from '../components/Header'
-import Main from '../components/Main'
-import Footer from '../components/Footer'
+// import Header from '../components/Header'
+import Link from 'next/link'
+import React from 'react'
+// import Main from '../components/Main'
+// import Footer from '../components/Footer'
 
-const Home: NextPage = () => {
+const Home = () => {
   return (
-    
+    <ul>
+      <li>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/About">
+          <a>About</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/Blog">
+          <a>Blog</a>
+        </Link>
+      </li>
+    </ul>
   )
 }
 
