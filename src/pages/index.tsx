f0fd8e32921356c89dfcf65bc8d01cdf64f9db28
87Prashant <87KumarPrashant@gmail.com>
2022-07-24 23:01:50 +0530

    added mui svg icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import Header from '../components/Header'
+import {Face} from '@mui/icons-material'
 
 export default function Home() {
   return (
     <div>
       <Header/>
+      <Face/>
       <div>This is the home Page</div>
     </div>
   );
