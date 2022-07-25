4c6964841bf9331ddf2fd5be4ea43b04f2068cc0
87Prashant <87KumarPrashant@gmail.com>
2022-07-25 23:22:28 +0530

    add header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,24 @@
 import Link from "next/link";
+import { Home, Boy, Article, DarkMode } from "@mui/icons-material";
 
 const Header = () => {
-    return (
-        <div>
+  return (
+    <div className="flex items-center border-4 border-black-800 justify-center sticky w-full h-20 z-50 top-0 mb-8 backdrop-blur-xl">
+      <div className="container gap-3 px-5 max-w-5xl flex flex-row justify-end">
+        <DarkMode className="cursor-pointer absolute mr-1200" />
         <Link href="/">
-        <a>Home</a>
-      </Link>
-      <Link href="/About">
-        <a>About</a>
-      </Link>
-      <Link href="/Blog">
-        <a>Blog</a>
-      </Link>
-      <hr/>
+          <Home className="cursor-pointer" />
+        </Link>
+        <Link href="/About">
+          <Boy className="cursor-pointer" />
+        </Link>
+        <Link href="/Blog">
+          <Article />
+        </Link>
+        <hr />
       </div>
-    )
-}
+    </div>
+  );
+};
 
-export default Header
+export default Header;
