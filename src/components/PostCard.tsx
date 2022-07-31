d1a7f6fb28d76994bd24741d0adaa205a380c507
87Prashant <87KumarPrashant@gmail.com>
2022-07-31 22:41:35 +0530

    style postCard

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -5,11 +5,11 @@ import type { Post } from "contentlayer/generated";
 
 const PostCard = (post: Post) => {
   return (
-    <div className="flex align-center justify-center transition-all duration-300 hover:bg-zinc-200 rounded w-full p-1.5">
+    <div className="flex align-center transition-all duration-300 hover:bg-zinc-200 rounded w-full p-4">
       <div>
         <time dateTime={post.date} className="block text-sm text-slate-600">
           {format(parseISO(post.date), "LLLL d, yyyy")}
-        </time>
+        </time> 
         <h2 className="text-lg">
           <Link href={post.url}>
             <a className="text-blue-700 hover:text-blue-900">{post.title}</a>
