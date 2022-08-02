44dcc2914962b02fadfab231d0193ae395513a07
87Prashant <87KumarPrashant@gmail.com>
2022-08-02 21:22:42 +0530

    resolve style issue in the home page

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import Image from 'next/image'
-import ProfilePic from '../Data/Images/Profile.png'
+import ProfilePic from '../Data/Images/Profile_Cropped.png'
 
 const Intro = () => {
   return (
     <div className="relative left-0 mb-10">
       <div className="flex flex-col z-10">
         <h1 className="text-2xl font-bold mb-6">Prashant Kumar</h1>
-        <p className="text-lg w-[80%]">I am from India and currently learning frontend development with React</p>
+        <p className="text-lg w-[70%]">I am from India and currently learning frontend development with React</p>
       </div>
-      <div className="absolute right-[-50px] top-0 flex align-center justify-center z-5">
-        <Image src={ProfilePic} height="100px" width="200px"></Image>
+      <div className="absolute right-0 top-0 flex align-center justify-center z-5">
+        <Image src={ProfilePic} height="100px" width="100px"></Image>
       </div>
     </div>
   );
