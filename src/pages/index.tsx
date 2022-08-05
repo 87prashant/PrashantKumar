300c40b01eea1d545c06768b5483d399b974716e
87Prashant <87KumarPrashant@gmail.com>
2022-08-05 06:51:42 +0530

    change font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ import type { Post } from "contentlayer/generated";
 import Intro from "../components/Intro";
 import Link from "next/link";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-import ProjectCard from '../components/ProjectCard'
+import ProjectCard from "../components/ProjectCard";
 
 export async function getStaticProps() {
   const posts = allPosts.sort((a, b) => {
@@ -29,29 +29,30 @@ export default function Home({ posts }: Props) {
       <Header />
       <Main>
         <Intro />
-        <section className="mb-6">
-        <div className="flex mb-4 items-center">
-          <h1 className="text-lg font-bold">Posts</h1>
-          <div className="flex ml-auto font-bold transition-all duration-200 bg-zinc-200 hover:bg-zinc-300 rounded-lg text-black cursor-pointer p-2">
-            <Link href="/Post">
-              <div className="flex">
-                Show All
-                <div className="flex items-center justify-center ml-3">
-                  <ArrowForwardIcon />
-                </div>
-              </div>
-            </Link>
+        <section>
+          <div className="font-bold text-lg mb-6">
+            Projects / Open-Source Contributions
           </div>
-        </div>
-        {posts.slice(0, 2).map((post, idx) => (
-          <PostCard key={idx} post={post} />
-        ))}
+          <ProjectCard />
         </section>
-        
-        <div className="font-bold text-lg mb-4">
-          Projects / Open-Source Contributions
-        </div>
-        <ProjectCard/>
+        <section className="mb-6">
+          <div className="flex mb-4 items-center">
+            <h1 className="text-lg font-bold">Posts</h1>
+            <div className="flex ml-auto font-bold transition-all duration-200 bg-zinc-200 hover:bg-zinc-300 rounded-lg text-black cursor-pointer p-2">
+              <Link href="/Post">
+                <div className="flex">
+                  Show All
+                  <div className="flex items-center justify-center ml-3">
+                    <ArrowForwardIcon />
+                  </div>
+                </div>
+              </Link>
+            </div>
+          </div>
+          {posts.slice(0, 2).map((post, idx) => (
+            <PostCard key={idx} post={post} />
+          ))}
+        </section>
       </Main>
       <Footer />
     </>
