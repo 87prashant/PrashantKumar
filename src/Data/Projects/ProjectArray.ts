9abaf9fe6ead2a3cbe90ace18f3cc7bb7d48b8f1
87Prashant <87KumarPrashant@gmail.com>
2022-10-02 23:02:55 +0530

    add tags for projects

--- src/Data/Projects/ProjectArray.ts
+++ src/Data/Projects/ProjectArray.ts
@@ -3,26 +3,38 @@ const ProjectArray = [
     title: "Personal Website",
     linkToWork: "https://github.com/87prashant/PrashantKumar",
     description: "Portfolio with blog/post functionality",
+    tags: ["TypeScript", "ReactJs", "NextJs", "TailwindCss", "MaterialUI"],
   },
   {
     title: "Parabol",
     linkToWork: "https://github.com/ParabolInc/parabol/pull/7154",
     description: "Fixed issues and added frontend features",
+    tags: [
+      "TypeScript",
+      "ReactJs",
+      "EmotionJs",
+      "Relay",
+      "DraftJs",
+      "MaterialUI",
+    ],
   },
   {
     title: "Minor Project",
     linkToWork: "https://github.com/87prashant/Minor-Pojects",
     description: "Basic projects with HTML, CSS and Vanilla JavaScript",
+    tags: ["HTML", "CSS", "Vanilla JavaScript"],
   },
   {
     title: "Desktop Notifier App",
     linkToWork: "https://github.com/87prashant/Desktop-Notifier-App",
     description: "Notifies about current COVID cases in India",
+    tags: ["Python"],
   },
   {
     title: "MindChart",
     linkToWork: "https://github.com/87prashant/MindChart",
     description: "Tool to create map of our Mind [WIP]",
+    tags: ["TypeScript", "ReactJs", "D3Js", "EmotionJs"],
   },
 ];
 
